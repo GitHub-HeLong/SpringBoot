590638c9630494e0370c41e9f3c5b463683d1493
GitHub-HeLong <786140193@qq.com>
2018-08-03 15:27:40 +0800

    添加jar包

--- src/main/java/ctrl/Ctrl.java
+++ src/main/java/ctrl/Ctrl.java
@@ -1,6 +1,18 @@
 package ctrl;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@EnableAutoConfiguration
 public class Ctrl {
+
+	@RequestMapping("/")
+	String home() {
+		/* @ComponentScan({ "main" }) */
+		// http://10.0.17.19:8080/test
+		return "Hello World!";
+	}
 
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,21 +1,22 @@
 package main;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableAutoConfiguration
+// 指定扫描的类
+@ComponentScan("dao")
 public class Main {
 
-	@RequestMapping("test")
-	String home() {
-		return "Hello World!";
-	}
+	public static void main(String[] args) throws Exception {
 
-	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
+		System.out.println("http://10.0.17.19:8080/test");
 	}
 
 }
